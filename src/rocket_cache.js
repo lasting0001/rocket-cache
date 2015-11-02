c6729069f72e1f4b094a82fccf486104bdd11629
li.j <li.j1@yuwan8.com>
2015-11-02 20:45:57 +0800

    优化缓存更新机制：使用预加载的形式，过期时先预返回内存，然后异步清除和加载数据，对于用户来说，永远是快速的返回内存数据

--- src/rocket_cache.js
+++ src/rocket_cache.js
@@ -119,15 +119,17 @@ global._RocketCache = function (opts) {
             }
             callBack(backResult);
         };
+        this.key_set && this.key_set.push(data_key);
+        // 预返回
+        this.stash.get(data_key, fetch, cb);
         // 数据是否已过期
         if ((from_db === true) || (opts.valid_time && (!scope.birth_times[data_key] || (scope.birth_times[data_key] + opts.valid_time < Date.now())))) {
+            // 过期删除
             this.del(data_key, function () {
-                scope.key_set && this.key_set.push(data_key);
-                scope.stash.get(data_key, fetch, cb);
+                // 预加载(用户每次都加载内存数据，提升响应度)
+                this.stash.get(data_key, fetch, function () {
+                });
             });
-        } else {
-            this.key_set && this.key_set.push(data_key);
-            this.stash.get(data_key, fetch, cb);
         }
     };
     RocketCache.prototype.clearType = function (callBack) {
